c0d95a1977d0c12bd0d5fc8f858f906cc64b8782
liuguanghui <1994218504@qq.com>
2022-10-26 14:46:19 +0800

    使用vuex来保存用户登录消息

--- vue2-demo/src/router/message.js
+++ vue2-demo/src/router/message.js
@@ -5,9 +5,14 @@ let routers = [
     component: () => import('../views/message/HomeView.vue'),
   },
   {
-    path: '/mainmessage',
-    name: 'mainmessage',
+    path: '/message/main',
+    name: 'messagemain',
     component: () => import('../views/message/MainView.vue'),
+  },
+  {
+    path: '/message/user',
+    name: 'messageuser',
+    component: () => import('../views/message/UserView.vue'),
   },
 ]
 export default routers
--- vue2-demo/src/views/HomeView.vue
+++ vue2-demo/src/views/HomeView.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
     首页
-    <a @click="toAbout()" href="javascript:viod(0)">关于</a>
+    <a v-for="p in pages" :key="p.path" @click="toPage(p.path)" href="javascript:viod(0)">{{ p.title }}</a>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      pages: [
+        { title: '关于', path: '/about' },
+        { title: '留言板首页', path: '/message/main' },
+        { title: 'websocket测试', path: '/test/ws' },
+      ],
+    }
+  },
   methods: {
     toAbout() {
       //编程式路由跳转
       this.$router.push('/about')
+    },
+    toPage(page) {
+      this.$router.push(page.path)
     },
   },
 }
